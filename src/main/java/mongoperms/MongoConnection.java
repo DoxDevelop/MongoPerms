80721da9a9cc63e792f27b16b69e48d4edc54bca
1bytee <cedric.kring@googlemail.com>
2016-05-07 18:55:44 +0200

    HotFix 1

--- src/main/java/mongoperms/MongoConnection.java
+++ src/main/java/mongoperms/MongoConnection.java
@@ -2,7 +2,6 @@ package mongoperms;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
@@ -76,7 +75,7 @@ public class MongoConnection {
         }
 
         collection.insertOne(new Document("group", group)
-                .append("permissions", Sets.newHashSet())
+                .append("permissions", Lists.newArrayList())
                 .append("inherits", Lists.newArrayList())
         );
 
